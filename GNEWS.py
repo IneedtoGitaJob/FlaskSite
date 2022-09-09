b34ed9284d7046d560aa8139e920ce776d9d6789
IneedtoGitaJob <84819013+IneedtoGitaJob@users.noreply.github.com>
2022-09-09 05:59:25 -0400

    Update GNEWS.py

--- GNEWS.py
+++ GNEWS.py
@@ -1,7 +1,6 @@
 from GoogleNews import GoogleNews
 import datetime
 import requests
-from nltk.sentiment import SentimentIntensityAnalyzer
 import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
